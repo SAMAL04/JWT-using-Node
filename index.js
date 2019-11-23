7cb0a985ed55a8f9ae5c1666dfcaceb98a8f867c
Sathiya Malleswar <56217973+SAMAL04@users.noreply.github.com>
2019-11-23 05:04:52 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+app.get('/api',(req,res)=>
+{
+    res.json({
+        message: "Welcome to API"
+    });
+});
+
+app.post('/api/posts',verifyToken,(req,res)=>
+{
+    jwt.verify(req.token,'securitykey',(err,authData)=>
+    {
+        if(err)
+        {
+            res.sendStatus(403);
+        }
+        else{
+            res.json(
+                {
+                    message:'Post is created',
+                    authData
+                }
+            );
+        }
+    })
+    
+});
+
+app.post('/api/login',(req,res)=>
+{
+    const user ={
+        id:1,
+        user:'sathiya',
+        email:'sathiya@SpeechGrammarList.com'
+    }
+    jwt.sign({user},'securitykey',(err,token)=>
+    {
+        res.json({
+            token
+        });
+    });
+});
+
+function verifyToken(req,res,next)
+{
+ const bearerHeader = req.headers['authorization'];
+ if(typeof(bearerHeader)!=='undefined')
+ {
+   const bearer = bearerHeader.split(' ');
+   const bearerToken = bearer[1];
+   req.token=bearerToken;
+   next();
+ }
+ else{
+     res.sendStatus(403);
+ }
+}
+app.listen(5000, ()=> console.log('Server started on 5000 port'));
